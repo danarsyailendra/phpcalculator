55f944531e936db0fbd6d50097c393662b7006a7
Danar Arga Syailendra <danarsyailendra17@gmail.com>
2022-06-28 22:30:22 +0700

    add operation controller

--- src/Http/Controller/CalculatorController.php
+++ src/Http/Controller/CalculatorController.php
@@ -2,10 +2,23 @@
 
 namespace Jakmall\Recruitment\Calculator\Http\Controller;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Jakmall\Recruitment\Calculator\Operation;
+
 class CalculatorController
 {
-    public function calculate()
+    public function calculate(Request $request, $action)
     {
-        dd('calculate result here');
+        $numbers = $request->input('input');
+
+        $operation = new Operation();
+        $result = $operation->call(strtolower($action), $numbers);
+        $arr_result = explode(" = ", $result);
+        return new Response([
+            "command" => strtolower($action),
+            "operation" => $arr_result[0],
+            "result" => (float)$arr_result[1]
+        ], 201);
     }
 }
